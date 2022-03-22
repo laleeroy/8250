8eefcec781971da8125f509bb8feab59107dda64
nullxception <enuma.alrizky@gmail.com>
2022-03-22 00:52:06 +0000

    spes: parts: Refactor dirac code
    [1] - Improve strings for dirac tile on/off
    [2] - Use proper singleton pattern for DiracUtils
    [3] - Describe current Dirac state to the tile
    
    Co-authored-by: chrisw444 <wandersonrodriguesf1@gmail.com>
    Change-Id: I7ec9548340882cb9196b48d444679e579ba13b8f
    Signed-off-by: Hoang Pham Anh Duy <duy@etylix.me>

--- parts/src/org/lineageos/settings/BootCompletedReceiver.java
+++ parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -34,10 +34,15 @@ public class BootCompletedReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(final Context context, Intent intent) {
-        if (DEBUG) Log.d(TAG, "Received boot completed intent");
+	if (DEBUG)
+            Log.d(TAG, "Received boot completed intent");
+        try {
+            DiracUtils.getInstance(context);
+        } catch (Exception e) {
+            Log.d(TAG, "Dirac is not present in system");
+        }
         DozeUtils.checkDozeService(context);
         PopupCameraUtils.checkPopupCameraService(context);
         ThermalUtils.startService(context);
-        DiracUtils.initialize(context);
     }
 }
